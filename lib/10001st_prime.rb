56822888ea52b0b2d50150713e0fc6009d8b6b34
dcarrano <dcarrano@gmail.com>
2016-01-24 02:01:18 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,31 @@
-# Implement your procedural solution here!
+def prime?(number)
+
+i = 2
+
+until (number % i) == 0 || i > Math.sqrt(number).floor
+  i += 1
+end
+
+if i > Math.sqrt(number).floor then true else false end
+
+end
+
+def prime_number_for(nth_element)
+
+  case
+  when nth_element < 1
+    answer = 0
+  when nth_element == 1
+    answer = 2
+  else
+    answer = 1
+    nth_prime = 1
+    until nth_prime == nth_element
+      answer += 2
+      if prime?(answer) then nth_prime += 1 end
+      end
+  end
+
+answer
+
+end
